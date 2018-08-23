4ce44b123f58104f1180982322631bf5f154cd38
1727738828 <1727738828@qq.com>
2018-08-23 14:19:26 +0800

    代码更新

--- controllers/changeSort.js
+++ controllers/changeSort.js
@@ -1,4 +1,14 @@
 exports.changeSort= function(req, res){
-
-    res.render('changeSort',{});
+    //1, 引入模块
+    var INDEX = require('../dao/dao_changeSort');
+    //2,创建对象
+    var sortName = new INDEX();
+    sortName.init();
+    //查询菜品分类
+    var sortNamesql = "select * from sort";
+    sortName.queryAll(sortNamesql, function (sortName) {
+        res.render('changeSort', {
+            sortName: sortName,
+        });
+    });
 };
