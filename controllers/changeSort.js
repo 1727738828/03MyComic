a9a19924eb1412468c6bb3b6bf2f1560f4959578
1727738828 <1727738828@qq.com>
2018-08-23 15:55:30 +0800

    代码更新

--- controllers/changeSort.js
+++ controllers/changeSort.js
@@ -1,14 +1,34 @@
-exports.changeSort= function(req, res){
+exports.changeSort = function (req, res) {
     //1, 引入模块
-    var INDEX = require('../dao/dao_changeSort');
+    var Sort = require('../dao/dao_changeSort');
     //2,创建对象
-    var sortName = new INDEX();
+    var sortName = new Sort();
     sortName.init();
-    //查询菜品分类
     var sortNamesql = "select * from sort";
     sortName.queryAll(sortNamesql, function (sortName) {
         res.render('changeSort', {
             sortName: sortName,
         });
     });
+
+    // 修改
+    var name = req.body.name;
+    var index = req.body.Index;
+
+    //2,创建对象
+    var sortUpdate = new Sort();
+    sortUpdate.init();
+
+    var sortUpdatesql="UPDATE sort SET name = '"+name+"' where id="+index;
+    sortUpdate.update(sortUpdatesql);
+// 删除
+    if (req.body.id != null) {
+        var sortID = req.body.id;
+        //2,创建对象
+        var deletesort = new Sort();
+        deletesort.init();
+        var deletesortsql = "delete from sort where id=" + sortID;
+        deletesort.queryAll(deletesortsql, function (deletesort) {
+        });
+    }
 };
